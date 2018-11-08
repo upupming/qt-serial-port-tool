162c83f1e9a95289720629c6fe2994d6aeb0af22
upupming <upupming@gmail.com>
2018-11-08 21:43:28 +0800

    Finished: change color according to displacement diff during period

--- SerialPortToolDemo/mainwindow.cpp
+++ SerialPortToolDemo/mainwindow.cpp
@@ -1,5 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "qmessagebox.h"
+#include<math.h>
+#include<QPalette>
+#include<chrono>
 
 #include <QSerialPort>
 #include <QSerialPortInfo>
@@ -8,6 +12,9 @@
 #include <sstream>
 #include <iomanip>
 #include <string>
+#include"xlsxdocument.h"
+#include <queue>
+
 using namespace std;
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -15,6 +22,9 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QTimer *timer = new QTimer(this);
+    connect(timer,SIGNAL(timeout()),this,SLOT(timerUpdate()));
+    timer->start(1000);
 
     //查找可用的串口
     QList<QSerialPortInfo> ports = QSerialPortInfo::availablePorts();
@@ -38,11 +48,29 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+void MainWindow::timerUpdate(void)
+{
+    QDateTime time = QDateTime::currentDateTime();
+    QString str = time.toString("yyyy-MM-dd hh:mm:ss dddd");
+    ui->timer->setText(str);
+    value = time.toString("yyyy-MM-dd");
+}
 
 void MainWindow::on_OpenSerialButton_clicked()
 {
     if(ui->OpenSerialButton->text() == tr("打开串口"))
     {
+        // Set color
+        ui->pushButton->setStyleSheet("QPushButton{background-image: url(:/new/prefix1/red.png);}");
+        beginTimeInited = false;
+        timeSinceBegin = 0;
+        for(int i=0; i<10; i++) {
+            priority_queue<double> emptyMaxHeap;
+            priority_queue<double, std::vector<double>, std::greater<double>> emptyMinHeap;
+            swap(displacementMaxHeaps[i], emptyMaxHeap);
+            swap(displacementMinHeaps[i], emptyMinHeap);
+        }
+
         serial = new QSerialPort;
         //设置串口名
         serial->setPortName(ui->PortBox->currentText());
@@ -111,6 +139,19 @@ void MainWindow::on_OpenSerialButton_clicked()
 
 }
 
+double MainWindow::getMaxMinDisplacementDiff() {
+    if(timeSinceBegin < 9) {
+        // No enough data yet, return 0
+        return 0;
+    } else {
+        // At time = 9, we return displacementMaxHeaps[0] - displacementMinHeaps[0]
+        // At time = 19, we return displacementMaxHeaps[0] - displacementMinHeaps[0], too
+        // It is also to say that timeSinceBegin must >=9
+        cout << timeSinceBegin << "   " << (timeSinceBegin-9)%10 << endl;
+        return displacementMaxHeaps[(timeSinceBegin-9)%10].top() - displacementMinHeaps[(timeSinceBegin-9)%10].top();
+    }
+}
+
 void MainWindow::calData() {
     dataUpdated = false;
 
@@ -119,21 +160,60 @@ void MainWindow::calData() {
     current[2] = stoi(bufStr.substr(4, 2));
     currentDisplacement = current[2] + current[1]/100.0 + current[0]/10000.0;
 
-    // Cal difference
-    if(previous[0] != 0 || previous[1] != 0 || previous[2] != 0) {
-        difference[0] = current[0] - previous[0];
-        difference[1] = current[1] - previous[1];
-        difference[2] = current[2] - previous[2];
-        differenceDisplacement = difference[2] + difference[1]/100.0 + difference[2]/10000.0;
+    // ######### Add to heaps for future query
+    // time = 5, save to displacementMaxHeaps[0] - displacementMaxHeaps[5]
+    // time = 0, save to displacementMaxHeaps[0] only
+    // time = 1, save to displacementMaxHeaps[0] and displacementMaxHeaps[1]
+    // time = 10, save to displacementMaxHeaps[1] - displacementMaxHeaps[10]
+    // time = 11, save to displacementMaxHeaps[2] - displacementMaxHeaps[11]
+    if(timeSinceBegin >= 10) {
+        for(int j = timeSinceBegin - 9; j<=timeSinceBegin; j++) {
+            displacementMaxHeaps[j%10].push(currentDisplacement);
+            displacementMinHeaps[j%10].push(currentDisplacement);
+        }
+    } else {
+        for(int j=0; j<=timeSinceBegin; j++) {
+            displacementMaxHeaps[j].push(currentDisplacement);
+            displacementMinHeaps[j].push(currentDisplacement);
+        }
     }
 
-    previousDisplacement = previous[2] + previous[1]/100.0 + previous[0]/10000.0;
-    previous[0] = current[0];
-    previous[1] = current[1];
-    previous[2] = current[2];
+    displayString = to_string(currentDisplacement) + '\t' + "Not calculated" + '\t' + to_string(timeSinceBegin) + '\t';
+
+    // Calculate Max-Min diff only per second
+    QTime now = QTime::currentTime();
+    if(!beginTimeInited) {
+        beginTime = now;
+        timeSinceBegin = 0;
+        beginTimeInited = true;
+    } else {
+        int msPassed = beginTime.msecsTo(now);
+        cout << msPassed << endl;
+        if(msPassed / 1000 > timeSinceBegin) {
+            timeSinceBegin = msPassed / 1000;
+            differenceDisplacement = getMaxMinDisplacementDiff();
+            displayString = to_string(currentDisplacement) + '\t' + to_string(differenceDisplacement) + "\t\t" + to_string(timeSinceBegin) + '\t';
+
+            if(timeSinceBegin >= 10) {
+                // Clean heaps
+                priority_queue<double> emptyMaxHeap;
+                priority_queue<double, std::vector<double>, std::greater<double>> emptyMinHeap;
+                swap(displacementMaxHeaps[timeSinceBegin%10], emptyMaxHeap);
+                swap(displacementMinHeaps[timeSinceBegin%10], emptyMinHeap);
+
+                // Change color
+                if(differenceDisplacement < 0.01) {
+                    ui->pushButton->setStyleSheet("QPushButton{background-image: url(:/new/prefix1/green.png);}");
+                } else {
+                    ui->pushButton->setStyleSheet("QPushButton{background-image: url(:/new/prefix1/red.png);}");
+                }
+            }
+        }
+    }
+
+
 
     dataUpdated = true;
-    displayString = to_string(currentDisplacement) + '\t' + to_string(previousDisplacement) + '\t' + to_string(differenceDisplacement);
 }
 
 string MainWindow::bufToString(QByteArray buf) {
@@ -151,19 +231,20 @@ string MainWindow::bufToString(QByteArray buf) {
 //读取接收到的信息
 void MainWindow::ReadData()
 {
+
     QByteArray buf;
     buf = serial->readAll();
     if(!buf.isEmpty())
     {
         if(!dataStarted) {
-            // 假定数据起始 aa 到达时 buf.size() == 1
             if(bufToString(buf) == "aa") {
                 cout << "Starting to accumulate buf..." << endl;
                 dataStarted = true;
                 sizeRead = 0;
             }
-        } else {
-            // 假定同一个 buf 里面不同时含有（两次测量的数据中的一部分）
+        }
+        else
+        {
             size4Buf += buf;
             sizeRead += buf.size();
 
@@ -176,7 +257,7 @@ void MainWindow::ReadData()
                     QString str = ui->textEdit->toPlainText();
 
                     if(!headerPrinted) {
-                        str = QString::fromStdString(string("Current") + "\t" + "Previous" + "\t" + "Difference" + "\n") + str;
+                        str = QString::fromStdString(string("Current") + "\t"  + "Difference" + "\t"  + "timeSinceBegin"+ "\t" + "\n") + str;
                         headerPrinted = true;
                     }
 
@@ -200,4 +281,35 @@ void MainWindow::ReadData()
 void MainWindow::on_SendButton_clicked()
 {
     serial->write(ui->textEdit_2->toPlainText().toLatin1());
+}
+
+void MainWindow::on_ExportButton_clicked()
+{
+
+    QXlsx::Document xlsx("data.xlsx");
+    NUM1= ui->lineEdit->text();
+    NUM2= ui->lineEdit_2->text();
+       //![0]
+       //![1]
+       xlsx.write("A1", " 日期 ");
+       xlsx.write("B1", "设备型号");
+       xlsx.write("C1", " 设备序列号 ");
+       xlsx.write("D1", true);
+       xlsx.write("E1", "位移数据");
+       xlsx.write("F1", "差值数据");
+       xlsx.write(i,1, value);
+      // xlsx.write("B2", "WT-X");
+     //  xlsx.write("C2", " 001 ");
+       xlsx.write(i,2, NUM1);
+       xlsx.write(i,3, NUM2);
+       xlsx.write(i,4, true);
+       xlsx.write(i,5, currentDisplacement);
+       xlsx.write(i,6, differenceDisplacement);
+       //![1]
+       //![2]
+
+       xlsx.save();
+       i++;
+       //![2]
+
 }
--- SerialPortToolDemo/mainwindow.h
+++ SerialPortToolDemo/mainwindow.h
@@ -1,10 +1,15 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
+#include <QTimer>
+#include <QDateTime>
+
 
 #include <QMainWindow>
 #include <QDebug>
 #include <QtSerialPort/QSerialPort>
 #include <QtSerialPort/QSerialPortInfo>
+#include"xlsxdocument.h"
+#include <queue>
 
 #include <string>
 using namespace std;
@@ -30,6 +35,9 @@ private slots:
 
     void calData();
     string bufToString(QByteArray buf);
+    double getMaxMinDisplacementDiff();
+
+    void on_ExportButton_clicked();
 
 private:
     Ui::MainWindow *ui;
@@ -39,14 +47,45 @@ private:
     // 10 59 13
     int previous[3] = {0}, current[3] = {0}, difference[3] = {0};
     // 13.5910
-    double previousDisplacement, currentDisplacement, differenceDisplacement;
+    double currentDisplacement, differenceDisplacement;
+
+    // Save all displacements in 10s as max heap and min heap
+    int periodSeconds = 10;
+    // displacementMaxHeaps[0] saves displacements in 0 - 9
+    // displacementMaxHeaps[1] saves displacements in 1 - 10
+    // ...
+    // displacementMaxHeaps[9] saves displacements in 9 - 18
+    // displacementMaxHeaps[10] saves displacements in 10 - 19
+    // ...
+    // at time = 9, use displacementMaxHeaps[0] for displacement in 0 - 9
+    // at time = 10, use displacementMaxHeaps[1] for displacement in 1 - 10
+    // if time = 55, I have to use displacementMaxHeaps[46] for displacement in 46 - 55
+    // so at time = 55, we don't need displacementMaxHeaps[0] - displacementMaxHeaps[45] any more, so we free them in this way:
+    // at time n+10, free displacementMaxHeaps[n]
+    // so we can use displacementMaxHeaps[n] again for displacementMaxHeaps[n+10]
+    // That means displacementMaxHeaps[0] will be free when time = 10, and be used to store displacementMaxHeaps[10]
+    // So we only need a array(displacementMaxHeaps) of size 10
+    // And displacementMaxHeaps[0] saves displacements in 0 - 9; 10 - 19; ...
+    priority_queue<double> displacementMaxHeaps[10];
+    priority_queue<double, std::vector<double>, std::greater<double>> displacementMinHeaps[10];
+    bool beginTimeInited = false;
+    QTime beginTime;
+    int timeSinceBegin = 0;
+
     string displayString;
+    QString value;
+    int NUM,sz,m,l;
+    QString NUM1;
+    QString NUM2;
+    int i=2;
     bool dataUpdated;
     bool headerPrinted = false;
     bool dataStarted = false;
     int sizeRead = 0;
-    // 存储 4 个 16 进制数
     QByteArray size4Buf = QByteArray();
+
+public slots:
+   void timerUpdate(void);
 };
 
 #endif // MAINWINDOW_H
